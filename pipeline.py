3aa2eceda108d4d195b238357e86c6bdf7afebd2
Cristian Dramisino <cristiandrami@libero.it>
2022-11-05 11:06:25 +0100

    tests process added

--- pipeline.py
+++ pipeline.py
@@ -3,5 +3,11 @@
 import shlex
 import subprocess
 
+#cloning 
 git_clone_args= 'git clone https://github.com/cristiandrami/agile_lab_4_toy_pipeline.git'
 subprocess.run(shlex.split(git_clone_args), stdout=subprocess.PIPE, stderr=None, stdin=subprocess.PIPE)
+
+
+#testing 
+tests_args = 'pytest -v'
+subprocess.run(shlex.split(tests_args), stdout=subprocess.PIPE, stderr=None, stdin=subprocess.PIPE)
